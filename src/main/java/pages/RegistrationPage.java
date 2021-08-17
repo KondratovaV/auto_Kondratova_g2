424ddcc2fc560d41360e954b5aa98eb740253a2e
Viktoriia Kondratova <vika.poludnenko@gmail.com>
2021-08-17 20:06:57 +0300

    task 1

---
+++ src/main/java/pages/RegistrationPage.java
@@ -0,0 +1,351 @@
+package pages;
+
+import org.junit.Assert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.Select;
+
+public class RegistrationPage extends MainPage {
+
+    public RegistrationPage(WebDriver webDriver) {
+        super(webDriver);
+    }
+
+    @FindBy(xpath = "//a[@class='login']")
+    public WebElement loginInput;
+
+    @FindBy(xpath = "//input[@id='email_create']")
+    public WebElement emailCreateInput;
+
+    @FindBy(name = "SubmitCreate")
+    public WebElement submitCreateButton;
+
+    @FindBy(id = "customer_firstname")
+    public WebElement customerFirstName;
+
+    @FindBy(id = "customer_lastname")
+    public WebElement customerLastName;
+
+    @FindBy(id = "email")
+    public WebElement emailInput;
+
+    @FindBy(id = "passwd")
+    public WebElement passwordInput;
+
+    @FindBy(xpath = "//input[@id='firstname']")
+    public WebElement firstNameInput;
+
+    @FindBy(xpath = "//input[@id='lastname']")
+    public WebElement lastNameInput;
+
+    @FindBy(xpath = "//div[@class='account_creation']//p[4]//input[@type='text']")
+    public WebElement streetInput;
+
+    @FindBy(xpath = "//input[@id='city']")
+    public WebElement cityInput;
+
+    @FindBy(id = "id_state")
+    public WebElement selectStateByIndex;
+
+    @FindBy(css = "#postcode")
+    public WebElement postCodeInput;
+
+    @FindBy(id = "id_country")
+    public WebElement idCountry;
+
+    @FindBy(xpath = "//input[@id='phone_mobile']")
+    public WebElement phoneMobileInput;
+
+    @FindBy(id = "alias")
+    public WebElement aliasInput;
+
+    @FindBy(xpath = "//button[@name='submitAccount']")
+    public WebElement submitAccountButton;
+
+    @FindBy(id = "center_column']/h1")
+    public WebElement titleRegisteredAccount;
+
+    @FindBy(className = "info-account")
+    public WebElement successfulRegistration;
+
+    /**
+     * Method open SignIn page
+     **/
+    public void openRegistrationPage() {
+        open("http://automationpractice.com/index.php?controller=authentication&back=my-account");
+    }
+
+    /**
+     * Method click to SignIn page
+     * */
+    public void clickToSignIn() {
+        try {
+            loginInput.click();
+            logger.info("Click to SignIn page");
+        } catch (Exception e) {
+            logger.error("Can't click to SignIn");
+            Assert.fail("Can't click to SignIn");
+        }
+    }
+
+    /**
+     * Method input email
+     * @param email
+     * */
+    public void inputEmailCreate(String email) {
+        try {
+            emailCreateInput.sendKeys(email);
+            logger.info("Input email " + email);
+        } catch (Exception e) {
+            logger.error("Can't input email " + email);
+            Assert.fail("Can't input email " + email);
+        }
+    }
+
+    /**
+     * Method submit button
+     * */
+
+    public void submitButtonCreate() {
+        try {
+            submitCreateButton.click();
+            logger.info("Click submit button");
+        } catch (Exception e) {
+            logger.error("Can't submit button");
+            Assert.fail("Can't submit button");
+        }
+    }
+
+    /**
+     * Method input First name
+     * @param FirstName
+     * */
+
+    public void inputFirstName(String FirstName) {
+        try {
+            customerFirstName.sendKeys(FirstName);
+            logger.info("Input customer firstname" + FirstName);
+        } catch (Exception e) {
+            logger.error("Can't input customer firstname");
+            Assert.fail("Can't input customer firstname");
+        }
+    }
+
+    /**
+     * Method input customer Last name
+     * @param LastName
+     * */
+
+    public void inputLastName(String LastName) {
+        try {
+            customerLastName.sendKeys(LastName);
+            logger.info("Input customer lastname" + LastName);
+        } catch (Exception e) {
+            logger.error("Can't input customer lastname");
+            Assert.fail("Can't input customer lastname");
+        }
+    }
+
+    /**
+     * Method reset emailInput
+     * @param email
+     * */
+
+    public void resetEmailInput(String email) {
+        try {
+            emailInput.clear();
+            emailInput.sendKeys(email);
+            logger.info("Input email " + email);
+        } catch (Exception e) {
+            logger.error("Can't input email");
+            Assert.fail("Can't input email");
+        }
+    }
+
+    /**
+     * Method input password
+     * @param password
+     * */
+
+    public void inputPassword(String password) {
+        try {
+            passwordInput.sendKeys(password);
+            logger.info("Input password");
+        } catch (Exception e) {
+            logger.error("Can't input password");
+            Assert.fail("Can't input password");
+        }
+    }
+
+    /**
+     * Method reset firstName
+     * @param firstName
+     * */
+
+    public void resetFirstName(String firstName) {
+        try {
+            firstNameInput.clear();
+            firstNameInput.sendKeys(firstName);
+            logger.info("Input firstname " + firstName);
+        } catch (Exception e) {
+            logger.error("Can't input firstname");
+            Assert.fail("Can't input firstname");
+        }
+    }
+
+    /**
+     * Method reset lastName
+     * @param lastName
+     * */
+
+    public void resetLastName(String lastName) {
+        try {
+            lastNameInput.clear();
+            lastNameInput.sendKeys(lastName);
+            logger.info("Input lastname " + lastName);
+        } catch (Exception e) {
+            logger.error("Can't input lastname");
+            Assert.fail("Can't input lastname");
+        }
+    }
+
+    /**
+     * Method input street
+     * @param street
+     * */
+
+    public void inputStreet(String street) {
+        try {
+            streetInput.sendKeys(street);
+            logger.info("Input street " + street);
+        } catch (Exception e) {
+            logger.error("Can't input street");
+            Assert.fail("Can't input street");
+        }
+    }
+
+    /**
+     * Method input city
+     * @param city
+     * */
+
+    public void inputCity(String city) {
+        try {
+            cityInput.sendKeys(city);
+            logger.info("Input city " + city);
+        } catch (Exception e) {
+            logger.error("Can't input city");
+            Assert.fail("Can't input city");
+        }
+    }
+
+    /**
+     * Method select city by index
+     * @param index
+     * */
+    public void selectCity(int index) {
+        try {
+            Select select = new Select(webDriver.findElement(By.id("id_state")));
+            select.selectByIndex(index);
+            logger.info("Find select city by " + index);
+        } catch (Exception e) {
+            logger.error("Can't find select city by " + index);
+        }
+    }
+
+    /**
+     * Method input postcode
+     * @param postcode
+     * */
+
+    public void inputPostCode(String postcode) {
+        try {
+            postCodeInput.sendKeys(postcode);
+            logger.info("Input postcode " + postcode);
+        } catch (Exception e) {
+            logger.error("Can't input postcode");
+            Assert.fail("Can't input postcode");
+        }
+    }
+
+
+    /**
+     * Method
+     * @param country
+     * */
+    public void selectCountry(String country) {
+        try {
+            Select select = new Select(webDriver.findElement(By.id("id_country")));
+            select.selectByVisibleText(country);
+            logger.info("Find select country " + country);
+        } catch (Exception e) {
+            logger.error("Can't find select country " + country);
+        }
+    }
+
+    /**
+     * Method input phoneMobile
+     * @param phoneMobile
+     * */
+
+    public void inputPhoneMobile(String phoneMobile) {
+        try {
+            phoneMobileInput.sendKeys(phoneMobile);
+            logger.info("Input phoneMobile " + phoneMobile);
+        } catch (Exception e) {
+            logger.error("Can't input phoneMobile");
+            Assert.fail("Can't input phoneMobile");
+        }
+    }
+
+    /**
+     * Method input alias
+     * @param alias
+     * */
+
+    public void inputAlias(String alias) {
+        try {
+            aliasInput.clear();
+            aliasInput.sendKeys(alias);
+            logger.info("Input alias " + alias);
+        } catch (Exception e) {
+            logger.error("Can't input alias");
+            Assert.fail("Can't input alias");
+        }
+    }
+
+    /**
+     * Method submit Account Button
+     * */
+
+    public void submitAccount() {
+        try {
+            submitAccountButton.click();
+            logger.info("Click submit account button");
+        } catch (Exception e) {
+            logger.error("Can't submit account button");
+            Assert.fail("Can't submit account button");
+        }
+    }
+
+    /**
+     * Method check successful registration
+     * */
+
+    public void checkRegistrationSuccessful() {
+        try {
+            Assert.assertEquals("Welcome to your account. Here you can manage all of your personal information and orders."
+                    , successfulRegistration.getText());
+            logger.info("Successful created account");
+        } catch (Exception e) {
+            logger.error("Enable to create account");
+            e.printStackTrace();
+            Assert.fail("Enable to create account");
+        }
+    }
+
+
+
+}
--- src/test/java/registrationTests/RegistrationTest.java
+++ src/test/java/registrationTests/RegistrationTest.java
@@ -7,6 +7,8 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.Select;
+import pages.MainPage;
+import pages.RegistrationPage;
 
 import java.io.File;
 import java.util.concurrent.TimeUnit;
@@ -17,85 +19,48 @@ public class RegistrationTest {
     private WebDriver webDriver;
     private Logger logger = Logger.getLogger(getClass());
 
+    public RegistrationPage registrationPage;
+    public MainPage mainPage;
+
+    String email = "12k3d3jn4qkwebr@umail.com";
+
+
     @Test
-    public void testRegistrationValid() {
+    public void testRegistration() {
         //Setup browser
         File chrome = new File("drivers/chromedriver.exe");
         System.setProperty("webdriver.chrome.driver", chrome.getAbsolutePath());
         webDriver = new ChromeDriver();
+        registrationPage = new RegistrationPage(webDriver);
         webDriver.manage().window().maximize();
         webDriver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 
-        try{
-            webDriver.get("http://automationpractice.com/index.php");
-            logger.info("Open url - http://automationpractice.com/index.php");
-        } catch (Exception e) {
-            logger.error("Can't open url - http://automationpractice.com/index.php");
-            Assert.fail("Can't open url - http://automationpractice.com/index.php");
-        }
-
         //Your personal info
-        String email = "12k334qwer@umail.com";
-        webDriver.findElement(By.xpath("//a[@class='login']")).click();
-//        webDriver.findElement(By.className("login")).click();
-//        webDriver.findElement(By.cssSelector(".login")).click();
-        logger.info("Click login - //a[@class='login']");
-        webDriver.findElement(By.xpath("//input[@id='email_create']"))
-                .sendKeys(email);
-        logger.info("Input email - " + email);
-        webDriver.findElement(By.name("SubmitCreate")).click();
-//        webDriver.findElement(By.name("button[name$='SubmitCreate']")).click();
-        logger.info("Click account create form");
-        webDriver.findElement(By.id("customer_firstname"))
-                .sendKeys("Vika");
-        logger.info("Input customer firstname - Vika");
-        webDriver.findElement(By.id("customer_lastname"))
-                .sendKeys("Konkhg");
-        logger.info("Input customer lastname - gjkl");
-        webDriver.findElement(By.id("email")).clear();
-        webDriver.findElement(By.id("email")).sendKeys(email);
-        logger.info("Input email - " + email);
-        webDriver.findElement(By.id("passwd"))
-                .sendKeys("1234qwer");
-        logger.info("Input password - 1234qwer");
+        registrationPage.openRegistrationPage();
+        registrationPage.inputEmailCreate(email);
+        registrationPage.submitButtonCreate();
 
-        //your address
-        webDriver.findElement(By.xpath("//input[@id='firstname']")).clear();
-        webDriver.findElement(By.xpath("//input[@id='firstname']")).sendKeys("Vika");
-        logger.info("Input first name - Vika");
-        webDriver.findElement(By.xpath("//input[@id='lastname']")).clear();
-        webDriver.findElement(By.xpath("//input[@id='lastname']")).sendKeys("Kondratova");
-        logger.info("input last name - Kondratova");
-        webDriver.findElement(By.xpath("//div[@class='account_creation']//p[4]//input[@type='text']"))
-                .sendKeys("street New York, 12");
-        webDriver.findElement(By.xpath("//input[@id='city']"))
-                .sendKeys("New York");
-        Select state = new Select(webDriver.findElement(By.id("id_state")));
-        state.selectByIndex(32);
-        logger.info("Select by index - 32");
-        webDriver.findElement(By.cssSelector("#postcode"))
-                .sendKeys("12345");
-        logger.info("Input post code - 12345");
-        webDriver.findElement(By.id("id_country")).isDisplayed();
-        Select country = new Select(webDriver.findElement(By.id("id_country")));
-        country.selectByVisibleText("United States");
-        logger.info("Select by visible text - United States");
-        webDriver.findElement(By.xpath("//input[@id='phone_mobile']"))
-                .sendKeys("1234567890");
-        logger.info("Input phone - 1234567890");
-        webDriver.findElement(By.xpath("//input[@id='alias']")).clear();
-        webDriver.findElement(By.xpath("//input[@id='alias']"))
-                .sendKeys(email);
-        logger.info("Input alias - " + email);
-        webDriver.findElement(By.xpath("//button[@name='submitAccount']")).click();
-        logger.info("Click button submit registration");
-//        webDriver.findElement(By.xpath("//div[@id='center_column']/h1")).isDisplayed();
-//        logger.info("Check h1 is displayed - //div[@id='center_column']/h1");
+        //another method
+        registrationPage.inputFirstName("Vika");
+        registrationPage.inputLastName("Kond");
+        registrationPage.resetEmailInput(email);
+        registrationPage.inputPassword("1234qwer");
+
+        //Your address
+        registrationPage.resetFirstName("Vika");
+        registrationPage.resetLastName("Kond");
+        registrationPage.inputStreet("street New York");
+        registrationPage.inputCity("New York");
+        registrationPage.selectCity(32);
+        registrationPage.inputPostCode("12345");
+        registrationPage.selectCountry("United States");
+        registrationPage.inputPhoneMobile("1234567890");
+        registrationPage.inputAlias(email);
+        registrationPage.submitAccount();
+        registrationPage.checkRegistrationSuccessful();
 
         //Close chromedriver
         webDriver.quit();
-
-
     }
 
 }
